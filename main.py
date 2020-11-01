d4fd5e471e30394c13ff7b143ca6fbf735056a79
Zbigniew Bednarczyk <zbigniew.bednarczyk@smartfactor.pl>
2020-11-01 17:08:14 +0100

    Dodanie komentarzy i uporządkowanie danych wejściowych

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-
 from PyPDF2 import PdfFileReader, PdfFileWriter
-
 from probki import utworz_liste_stron
 
-nazwa_wypracowania = "wypracowani_z_Adama_Mickiewicza_Dziady_"
 
+# dane wejściowe: nazwa wypracowania - pisana z '_' pomiędzy wyrazami
+nazwa_wypracowania = "wypracowani_z_Adama_Mickiewicza_Dziady"
+
+# dane wejściowe: lista list zawierająca nazwisko i imię ucznia, zakres stron (może być 1-3, albo tylko 3)
+# ilość stron musi być identyczna jak w dokumencie wejściowym, strony numerujemy od 1, ostatni uczeń musi mieć końcową stronę
 lista_uczniow_i_stron_wejscie = [["Węgłowska Natalia", "1-2"], ["Witek Ewa", "3-4"], ["Sowa Adam", "5"], ["Gal Anonim", "6"]]
+
+# dane wejściowe: pomiędzy cudzysłowy należy wstawić nazwę pliku pdf z rozszerzeniem, np CCF25102020.pdf
+wejsciowy_pdf = "CCF25102020.pdf"
+
+
 
 
 # wstawiam '_' pomiędzy nazwisko i imię
@@ -24,15 +31,15 @@ def zrob_nazwy_plikow(lista_wejsciowa, nazwa_wypracowania):
 
 def pdf_splitter(wejsciowy_pdf, nazwa_wypracowania, lista_uczniow_i_stron):
     nazwa_wypracowania_wewnatrzna = zrob_nazwy_plikow(lista_uczniow_i_stron, nazwa_wypracowania)
-    print("nazwa_wypracowania_wewnatrzna", nazwa_wypracowania_wewnatrzna)
+    # print("nazwa_wypracowania_wewnatrzna", nazwa_wypracowania_wewnatrzna)
     pdf = PdfFileReader(wejsciowy_pdf)
 
     lista_stron_wszystkich_uczniow = utworz_liste_stron(lista_uczniow_i_stron)
-    print("lista_stron_wszystkich_uczniow", lista_stron_wszystkich_uczniow)
-    print("\n")
+    # print("lista_stron_wszystkich_uczniow", lista_stron_wszystkich_uczniow)
+    # print("\n")
 
     for indeks, wypracowanie in enumerate(nazwa_wypracowania_wewnatrzna):
-        print("Pierwszy for, indeks ", indeks, wypracowanie)
+        # print("Pierwszy for, indeks ", indeks, wypracowanie)
         pdf_writer = PdfFileWriter()
 
         # add.Page musi dodawać wszystkie strony z zakresu dla danej osoby - wykonany tyle razy na
@@ -40,7 +47,7 @@ def pdf_splitter(wejsciowy_pdf, nazwa_wypracowania, lista_uczniow_i_stron):
 
 
         for i in lista_stron_wszystkich_uczniow[indeks]:
-            print(f"lista_stron_wszystkich_uczniow i = {i}, {lista_stron_wszystkich_uczniow[indeks]}")
+            # print(f"lista_stron_wszystkich_uczniow i = {i}, {lista_stron_wszystkich_uczniow[indeks]}")
 
             pdf_writer.addPage(pdf.getPage(i))
 
@@ -52,8 +59,12 @@ def pdf_splitter(wejsciowy_pdf, nazwa_wypracowania, lista_uczniow_i_stron):
             output_filename = str(nazwa_wypracowania_wewnatrzna[indeks][0])+".pdf"
             with open(output_filename, 'wb') as out:
                 pdf_writer.write(out)
-            print(f'Utworzyłem {output_filename}')
-
-        print("\n")
-
-pdf_splitter("CCF25102020.pdf", nazwa_wypracowania, lista_uczniow_i_stron)
+#             print(f'Utworzyłem {output_filename}')
+#
+#         print("\n")
+#
+try:
+    pdf_splitter(wejsciowy_pdf, nazwa_wypracowania, lista_uczniow_i_stron)
+    print("Program zadziałał poprawnie!")
+except:
+    print("Wystąpił błąd w programie")
